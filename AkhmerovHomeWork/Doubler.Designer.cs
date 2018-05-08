cc5e859cba20d6064047887d88b426994d466a88
Zerohout <zerohout@gmail.com>
2018-05-08 14:15:01 +0300

    Add Doubler Interface

--- AkhmerovHomeWork/Doubler.Designer.cs
+++ AkhmerovHomeWork/Doubler.Designer.cs
@@ -28,22 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fDoubler));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUndoRemain = new System.Windows.Forms.Label();
+            this.lblUndoCount = new System.Windows.Forms.Label();
+            this.lblUserNum = new System.Windows.Forms.Label();
+            this.lblTargetNum = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStartAgain = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(667, 96);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = resources.GetString("lblTitle.Text");
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(257, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ВАШЕ ЧИСЛО:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label2.Location = new System.Drawing.Point(471, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ЦЕЛЕВОЕ ЧИСЛО:";
+            // 
+            // lblUndoRemain
+            // 
+            this.lblUndoRemain.AutoSize = true;
+            this.lblUndoRemain.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUndoRemain.ForeColor = System.Drawing.Color.Green;
+            this.lblUndoRemain.Location = new System.Drawing.Point(12, 126);
+            this.lblUndoRemain.Name = "lblUndoRemain";
+            this.lblUndoRemain.Size = new System.Drawing.Size(175, 23);
+            this.lblUndoRemain.TabIndex = 3;
+            this.lblUndoRemain.Text = "ОСТАЛОСЬ ОТМЕН:";
+            // 
+            // lblUndoCount
+            // 
+            this.lblUndoCount.AutoSize = true;
+            this.lblUndoCount.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUndoCount.ForeColor = System.Drawing.Color.Black;
+            this.lblUndoCount.Location = new System.Drawing.Point(193, 126);
+            this.lblUndoCount.Name = "lblUndoCount";
+            this.lblUndoCount.Size = new System.Drawing.Size(20, 23);
+            this.lblUndoCount.TabIndex = 4;
+            this.lblUndoCount.Text = "5";
+            this.lblUndoCount.TextChanged += new System.EventHandler(this.lblUndoCount_TextChanged);
+            // 
+            // lblUserNum
+            // 
+            this.lblUserNum.AutoSize = true;
+            this.lblUserNum.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUserNum.Location = new System.Drawing.Point(391, 126);
+            this.lblUserNum.Name = "lblUserNum";
+            this.lblUserNum.Size = new System.Drawing.Size(20, 23);
+            this.lblUserNum.TabIndex = 5;
+            this.lblUserNum.Text = "1";
+            // 
+            // lblTargetNum
+            // 
+            this.lblTargetNum.AutoSize = true;
+            this.lblTargetNum.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTargetNum.Location = new System.Drawing.Point(635, 126);
+            this.lblTargetNum.Name = "lblTargetNum";
+            this.lblTargetNum.Size = new System.Drawing.Size(20, 23);
+            this.lblTargetNum.TabIndex = 6;
+            this.lblTargetNum.Text = "1";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlus.Location = new System.Drawing.Point(207, 194);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(121, 59);
+            this.btnPlus.TabIndex = 7;
+            this.btnPlus.Text = "Прибавить";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMulti.Location = new System.Drawing.Point(334, 194);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(121, 59);
+            this.btnMulti.TabIndex = 8;
+            this.btnMulti.Text = "Умножить";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUndo.Location = new System.Drawing.Point(67, 194);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(120, 59);
+            this.btnUndo.TabIndex = 9;
+            this.btnUndo.Text = "Отменить";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBack.Location = new System.Drawing.Point(207, 350);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(248, 41);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Вернуться в Главное меню";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(475, 194);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(121, 59);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnStartAgain
+            // 
+            this.btnStartAgain.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStartAgain.Location = new System.Drawing.Point(207, 303);
+            this.btnStartAgain.Name = "btnStartAgain";
+            this.btnStartAgain.Size = new System.Drawing.Size(248, 41);
+            this.btnStartAgain.TabIndex = 12;
+            this.btnStartAgain.Text = "Начать/Сначала";
+            this.btnStartAgain.UseVisualStyleBackColor = true;
+            this.btnStartAgain.Click += new System.EventHandler(this.btnStartAgain_Click);
             // 
             // fDoubler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 412);
             this.ControlBox = false;
+            this.Controls.Add(this.btnStartAgain);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.lblTargetNum);
+            this.Controls.Add(this.lblUserNum);
+            this.Controls.Add(this.lblUndoCount);
+            this.Controls.Add(this.lblUndoRemain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "fDoubler";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Doubler";
+            this.Load += new System.EventHandler(this.fDoubler_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUndoRemain;
+        private System.Windows.Forms.Label lblUndoCount;
+        private System.Windows.Forms.Label lblUserNum;
+        private System.Windows.Forms.Label lblTargetNum;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnStartAgain;
     }
 }
--- AkhmerovHomeWork/Doubler.cs
+++ AkhmerovHomeWork/Doubler.cs
@@ -10,11 +10,122 @@ using System.Windows.Forms;
 
 namespace AkhmerovHomeWork
 {
+   
     public partial class fDoubler : Form
     {
+        static int userNum = 1;
+        static int globalCount = 0;
+        static int undoCount = 0;
+        static List<int> undoNum = new List<int>();
         public fDoubler()
         {
             InitializeComponent();
+        }
+
+        private void fDoubler_Load(object sender, EventArgs e)
+        {
+            Tumbler(false);
+            btnStartAgain.Text = "Начать";
+            
+
+
+        }
+
+
+
+        private void lblUndoCount_TextChanged(object sender, EventArgs e)
+        {
+            if (int.Parse(lblUndoCount.Text) > 3)
+            {
+                lblUndoRemain.ForeColor = Color.Green;
+            }
+            else if (int.Parse(lblUndoCount.Text) < 3)
+            {
+                lblUndoRemain.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblUndoRemain.ForeColor = Color.DarkOrange;
+            }
+
+            if ()
+        }
+
+        void Tumbler(bool status)
+        {
+            if (status)
+            {
+                btnUndo.Show();
+                btnPlus.Show();
+                btnMulti.Show();
+                btnReset.Show();
+                lblUndoCount.Show();
+                lblTargetNum.Show();
+                lblUserNum.Show();
+            }
+            else
+            {
+                btnUndo.Hide();
+                btnPlus.Hide();
+                btnMulti.Hide();
+                btnReset.Hide();
+                lblUndoCount.Hide();
+                lblTargetNum.Hide();
+                lblUserNum.Hide();
+            }
+        }
+
+        private void btnStartAgain_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            
+            if (btnStartAgain.Text == "Начать")
+            {
+                Tumbler(true);
+                btnStartAgain.Text = "Сначала";
+
+            }
+            else
+            {
+                userNum = 1;
+                lblUserNum.Text = $"{userNum}";
+            }
+
+            lblTargetNum.Text = $"{rnd.Next(5, 101)}";
+            undoNum.Clear();
+            globalCount = 0;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Tumbler(false);
+            btnStartAgain.Text = "Начать";
+            lblUndoCount.Text = "5";
+            undoNum.Clear();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            globalCount++;
+            undoCount++;
+            undoNum.Add(userNum);
+            userNum++;
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            globalCount++;
+            undoCount++;
+            undoNum.Add(userNum);
+            userNum *= 2;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            globalCount++;
+            undoCount++;
+            undoNum.Add(userNum);
+            userNum = 1;
         }
     }
 }
